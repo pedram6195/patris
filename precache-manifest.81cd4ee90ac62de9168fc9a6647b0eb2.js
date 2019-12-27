750b7825203b4e53ce3d5f3871f430dcc84fb300
pedram afrasiabi <pedram6195@yahoo.com>
2019-12-27 18:27:30 +0330

    Updates

---
+++ precache-manifest.81cd4ee90ac62de9168fc9a6647b0eb2.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf7fd458f77e87640b9ec75a647a1db",
+    "url": "/patris/index.html"
+  },
+  {
+    "revision": "606e4e93cc519f156152",
+    "url": "/patris/static/css/2.bc9ccb5a.chunk.css"
+  },
+  {
+    "revision": "abff08bc6ef1c0377996",
+    "url": "/patris/static/css/main.c56f1086.chunk.css"
+  },
+  {
+    "revision": "606e4e93cc519f156152",
+    "url": "/patris/static/js/2.03c09d39.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/patris/static/js/2.03c09d39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "abff08bc6ef1c0377996",
+    "url": "/patris/static/js/main.848e97e0.chunk.js"
+  },
+  {
+    "revision": "9aec4cca0eef967888e0",
+    "url": "/patris/static/js/runtime-main.56f76918.js"
+  },
+  {
+    "revision": "ac22d187130d6c3433a49a1e98bfa968",
+    "url": "/patris/static/media/IRANSansWeb.ac22d187.ttf"
+  },
+  {
+    "revision": "c6f46673e2449eb071e2c098c5190c2a",
+    "url": "/patris/static/media/IRANSansWeb.c6f46673.woff2"
+  },
+  {
+    "revision": "df14582918ca379a280e453bb3cc6ba5",
+    "url": "/patris/static/media/IRANSansWeb.df145829.woff"
+  },
+  {
+    "revision": "e43cfbc1a67d90e910398ded8345cd32",
+    "url": "/patris/static/media/IRANSansWeb.e43cfbc1.eot"
+  },
+  {
+    "revision": "4ff441e3b8c51acd2261585feca1d8c8",
+    "url": "/patris/static/media/IRANSansWeb_Bold.4ff441e3.ttf"
+  },
+  {
+    "revision": "6ec03a37e370c0d7f4f6bbdba0feac23",
+    "url": "/patris/static/media/IRANSansWeb_Bold.6ec03a37.woff2"
+  },
+  {
+    "revision": "7626d015e15844db7d1f54c8e415461a",
+    "url": "/patris/static/media/IRANSansWeb_Bold.7626d015.eot"
+  },
+  {
+    "revision": "a762a767a3459fec2195da1373ebf0b1",
+    "url": "/patris/static/media/IRANSansWeb_Bold.a762a767.woff"
+  },
+  {
+    "revision": "414e7148a634bfc82a601e3dacf84e96",
+    "url": "/patris/static/media/IRANSansWeb_Light.414e7148.woff2"
+  },
+  {
+    "revision": "469a8f6aa5b4631045b7d922102392ac",
+    "url": "/patris/static/media/IRANSansWeb_Light.469a8f6a.woff"
+  },
+  {
+    "revision": "53a90b39dae08e4e4539ca2ce0cb3a68",
+    "url": "/patris/static/media/IRANSansWeb_Light.53a90b39.ttf"
+  },
+  {
+    "revision": "60f95f28d8469e5df0398ba1d3e9708e",
+    "url": "/patris/static/media/IRANSansWeb_Light.60f95f28.eot"
+  },
+  {
+    "revision": "1121c0520a30a7295ffe21dd3fe77a60",
+    "url": "/patris/static/media/IRANSansWeb_Medium.1121c052.eot"
+  },
+  {
+    "revision": "74e2ab212452657f2fc66bd08873884b",
+    "url": "/patris/static/media/IRANSansWeb_Medium.74e2ab21.woff"
+  },
+  {
+    "revision": "7a75459b25fca9937df9110d7e049133",
+    "url": "/patris/static/media/IRANSansWeb_Medium.7a75459b.ttf"
+  },
+  {
+    "revision": "bb6a9fe5edb6791184821d992bffdcf4",
+    "url": "/patris/static/media/IRANSansWeb_Medium.bb6a9fe5.woff2"
+  },
+  {
+    "revision": "153b7b5a1579526611373210b6fd8754",
+    "url": "/patris/static/media/IRANSansWeb_UltraLight.153b7b5a.woff"
+  },
+  {
+    "revision": "5010d0152420ca7bd3acb1a9d668a57e",
+    "url": "/patris/static/media/IRANSansWeb_UltraLight.5010d015.woff2"
+  },
+  {
+    "revision": "b8efb89efe7dc02c439a362d17c9dc5e",
+    "url": "/patris/static/media/IRANSansWeb_UltraLight.b8efb89e.eot"
+  },
+  {
+    "revision": "eeb1edcad9b20c19bd1eecd1c371e4f8",
+    "url": "/patris/static/media/IRANSansWeb_UltraLight.eeb1edca.ttf"
+  },
+  {
+    "revision": "d521834532e600de49c0b1cfbc290b57",
+    "url": "/patris/static/media/Layer-16.d5218345.png"
+  },
+  {
+    "revision": "02c0037909a2d6e127fb168b27414f63",
+    "url": "/patris/static/media/Layer-17.02c00379.png"
+  },
+  {
+    "revision": "32356659f466b650f8a643f28226a7a6",
+    "url": "/patris/static/media/Layer-18.32356659.png"
+  },
+  {
+    "revision": "ec62837c7b1a88b04aa37d1ddbddf155",
+    "url": "/patris/static/media/Layer-19.ec62837c.png"
+  },
+  {
+    "revision": "dc6855e501d8b9ddf1046b764592349d",
+    "url": "/patris/static/media/Layer-21.dc6855e5.png"
+  },
+  {
+    "revision": "c84da3401646289ce1e639b7714ffb6e",
+    "url": "/patris/static/media/Layer-22.c84da340.png"
+  },
+  {
+    "revision": "4b3b58ea8407595f6cb45a22303e182d",
+    "url": "/patris/static/media/comment.4b3b58ea.png"
+  },
+  {
+    "revision": "7209c395872769f6454fe41737cfd387",
+    "url": "/patris/static/media/img.7209c395.png"
+  },
+  {
+    "revision": "eb2e242dc38851fd25cc760ef356f3eb",
+    "url": "/patris/static/media/img2.eb2e242d.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/patris/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/patris/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/patris/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/patris/static/media/slick.f97e3bbf.svg"
+  }
+]);
